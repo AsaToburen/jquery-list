cfb0dc6482f34ebb6273a4bcfa1c28bba5d2379c
Asa Toburen <atopettit@gmail.com>
2014-11-19 12:51:23 -0700

    fixed remove/delete using this, need to find a way to apply changes to new items.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
-	
 	$('button').click('input', function () {
 		
 		var shoppingItem = $( 'input' ).val();
 		
 		$('#theList').prepend('<ul class="entry"><li class="remove"></li><li class="item"><p>' + shoppingItem  +'</p></li><li class="cross-off"></li></ul>'); 
-		}) 
-			$(".cross-off").click(function() {
-				$(".item").find("p").addClass("finished");
-		}) 
-			$(".remove").click(".entry", function() {
-				$(".entry").hide();
-			});
+			}) 
 		});
-		
-	
-	
+
+$(document).ready(function(){
+
+			$(".cross-off").on('click', function() {
+				$(this).closest(".entry").find("p").toggleClass("finished");
+		}) 
+
+			$(".remove").on('click', function() {
+			$(this).closest(".entry").remove();
+			});
+			
+
+	});
 
 
